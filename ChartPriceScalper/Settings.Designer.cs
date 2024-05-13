5aa30293d223f2d505e2849a4620476bd991ab7b
Kevin <168022299+sharpjug@users.noreply.github.com>
2024-05-13 21:50:57 +0100

    Add files via upload

---
+++ ChartPriceScalper/Settings.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace ChartPriceScalper
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.cBoxTop = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listExchange = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSoundTest = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblSound = new System.Windows.Forms.Label();
+            this.listSound = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.headerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.Black;
+            this.headerPanel.Controls.Add(this.btnClose);
+            this.headerPanel.Controls.Add(this.lblTitle);
+            this.headerPanel.Controls.Add(this.btnExit);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(253, 38);
+            this.headerPanel.TabIndex = 20;
+            this.headerPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.headerPanel_MouseDown);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.Black;
+            this.btnClose.BackgroundImage = global::ChartPriceScalper.Properties.Resources.X;
+            this.btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(215, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(38, 38);
+            this.btnClose.TabIndex = 21;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Black;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(10, 8);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(84, 25);
+            this.lblTitle.TabIndex = 20;
+            this.lblTitle.Text = "Settings";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExit.BackColor = System.Drawing.Color.Black;
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Location = new System.Drawing.Point(219, 0);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(34, 35);
+            this.btnExit.TabIndex = 20;
+            this.btnExit.UseVisualStyleBackColor = false;
+            // 
+            // cBoxTop
+            // 
+            this.cBoxTop.AutoSize = true;
+            this.cBoxTop.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.cBoxTop.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.cBoxTop.Location = new System.Drawing.Point(9, 59);
+            this.cBoxTop.Name = "cBoxTop";
+            this.cBoxTop.Size = new System.Drawing.Size(77, 19);
+            this.cBoxTop.TabIndex = 32;
+            this.cBoxTop.Text = "Top Most";
+            this.cBoxTop.UseVisualStyleBackColor = true;
+            this.cBoxTop.CheckedChanged += new System.EventHandler(this.cBoxTop_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Location = new System.Drawing.Point(9, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Basic settings";
+            // 
+            // listExchange
+            // 
+            this.listExchange.FormattingEnabled = true;
+            this.listExchange.Location = new System.Drawing.Point(9, 84);
+            this.listExchange.Name = "listExchange";
+            this.listExchange.Size = new System.Drawing.Size(120, 30);
+            this.listExchange.TabIndex = 36;
+            this.listExchange.SelectedIndexChanged += new System.EventHandler(this.listExchange_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Location = new System.Drawing.Point(8, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 15);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "Sound settings";
+            // 
+            // btnSoundTest
+            // 
+            this.btnSoundTest.BackColor = System.Drawing.Color.Gray;
+            this.btnSoundTest.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSoundTest.Location = new System.Drawing.Point(189, 213);
+            this.btnSoundTest.Name = "btnSoundTest";
+            this.btnSoundTest.Size = new System.Drawing.Size(20, 20);
+            this.btnSoundTest.TabIndex = 44;
+            this.btnSoundTest.UseVisualStyleBackColor = false;
+            this.btnSoundTest.Click += new System.EventHandler(this.btnSoundTest_Click);
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label8.Location = new System.Drawing.Point(9, 144);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 43;
+            this.label8.Text = "Sound:\r\n";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSound
+            // 
+            this.lblSound.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSound.BackColor = System.Drawing.Color.Transparent;
+            this.lblSound.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSound.Font = new System.Drawing.Font("Segoe UI", 5.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSound.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.lblSound.Location = new System.Drawing.Point(9, 213);
+            this.lblSound.Name = "lblSound";
+            this.lblSound.Size = new System.Drawing.Size(135, 20);
+            this.lblSound.TabIndex = 42;
+            this.lblSound.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listSound
+            // 
+            this.listSound.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.listSound.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listSound.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listSound.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.listSound.FormattingEnabled = true;
+            this.listSound.ItemHeight = 9;
+            this.listSound.Location = new System.Drawing.Point(54, 144);
+            this.listSound.Name = "listSound";
+            this.listSound.Size = new System.Drawing.Size(109, 56);
+            this.listSound.TabIndex = 41;
+            this.listSound.SelectedIndexChanged += new System.EventHandler(this.listSound_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Location = new System.Drawing.Point(150, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 45;
+            this.label3.Text = "Test :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(253, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSoundTest);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblSound);
+            this.Controls.Add(this.listSound);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listExchange);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cBoxTop);
+            this.Controls.Add(this.headerPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Settings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Settings";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Settings_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Settings_MouseDown);
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel headerPanel;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.CheckBox cBoxTop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listExchange;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSoundTest;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblSound;
+        private System.Windows.Forms.ListBox listSound;
+        private System.Windows.Forms.Label label3;
+    }
+}
